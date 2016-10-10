dfdc12c7f0e7a766261d4b5a798bcd83ce16250c
Philipp Herkert <philipp.herkert@gmail.com>
2016-10-11 01:13:41 +0200

    Initial Commit

---
+++ backgammon2/src/main/java/bg/backgammon2/App.java
@@ -0,0 +1,13 @@
+package bg.backgammon2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
